8e927a60ddececdf6c1b1862a3c3f954481a1813
Dipankar Sarma <sarmadeep123@gmail.com>
2024-08-05 21:40:20 +0530

    Add all files

---
+++ src/app/courses/pages.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Courses() {
+  return (
+    <div className='flex justify-between items-center'>
+      <h1 className='text-center text-white'>Mucis Course</h1>
+    </div>
+  )
+}
+
+export default Courses
